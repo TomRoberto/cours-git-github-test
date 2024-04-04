dee94337963c64712e1f4510657cedd79fb415b3
BastienLairis <bastien.lairis@gmail.com>
2024-04-04 14:43:06 +0200

    Add Footer Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <h4>Hey</h4>
       <h3>hhihihihihi</h3>
       <h5>Modif sur main je rajoute un truc sur GitHub</h5>
+      <Footer />
     </>
   );
 }
